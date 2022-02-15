7c70ffdd5213cc4831aba143271fafe50d6bbec0
Reginald Appiah <reginald.appiah7@gmail.com>
2022-02-15 09:56:30 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+enum TileState { covered, blown, open, flagged, revealed }
+
+void main() {
+  runApp(MineSweeper());
+}
+
+class MineSweeper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minesweeper',
+      home: Board(),
+    );
+  }
+}
+
+class Board extends StatefulWidget {
+  Board({Key? key}) : super(key: key);
+  @override
+  _BoardState createState() => _BoardState();
+}
+
+class _BoardState extends State<Board> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Text("Board"));
+  }
+}
